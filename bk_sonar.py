dfb99d330103bd92b23f279d13185491aed78708
bdxbot <bdxbot@gmail.com>
2021-07-29 03:11:34 -0700

    i typed this up now

--- bk_sonar.py
+++ bk_sonar.py
@@ -19,7 +19,7 @@ def getNewBoard():
 
 def drawBoard(board):
 	# Draw the board data structure.
-	tensDigitsLine = '	' # Initial space for the numbers down the left side of the board
+	tensDigitsLine = '    ' # Initial space for the numbers down the left side of the board
 	for i in range(1, 6):
 		tensDigitsLine += (' ' * 9) + str(i)
 
@@ -55,9 +55,164 @@ def getRandomChests(numChests):
 		newChest = [random.randint(0, 59), random.randint(0, 14)]
 		if newChest not in chests: # Make sure a chest is not already here!
 			chests.append(newChest)
-	return numChests
+	return chests
 
 def isOnBoard(x, y):
 	# Return True if the coordinates are on the board: otherwise, return False.
 	return x >= 0 and x <= 59 and y >= 0 and y <= 14
 
+def makeMove(board, chests, x, y):
+	# Change the board data structure with a sonar device character.
+	# Remove treasure chests from the chests list as tehy are found.
+	# Return False if this is an invalid move.
+	# Otherwise, return the string of the result of this move.
+	smallestDistance = 100 # Any chest will be closer than 100.
+	for cx, cy in chests:
+		distance = math.sqrt((cx - x) * (cx - x) + (cy - y) * (cy - y))
+
+		if distance < smallestDistance: # We want the closest treasure chest.
+			smallestDistance = distance
+
+	smallestDistance = round(smallestDistance)
+
+	if smallestDistance == 0:
+		# xy is directly on a treasure chest!
+		chests.remove([x, y])
+		return 'You have found a sunken treasure chest!'
+	else:
+		if smallestDistance < 10:
+			board[x][y] = str(smallestDistance)
+			return '\nTreasure detected at a distance of %s from the sonar \
+beacon.' % (smallestDistance)
+		else:
+			board[x][y] = 'X'
+			return 'Sonar beacon did not detect anything. All treasure chests \
+out of range'
+
+def enterPlayerMove(previousMoves):
+	# Let the player enter their move. Return a two-item list of int xy coords.
+	print('Where do you want to drop the next sonar beacon? (0-59 0-14) \
+(or type quit)')
+	while True:
+		move = input()
+		if move.lower() == 'quit':
+			print('Thanks for playing!')
+			sys.exit()
+
+		move = move.split()
+		if len(move) == 2 and move[0].isdigit() and move[1].isdigit() and \
+		isOnBoard(int(move[0]), int(move[1])):
+			if [int(move[0]), int(move[1])] in previousMoves:
+				print('You already searched there.')
+				continue
+			return [int(move[0]), int(move[1])]
+
+		print('Enter a number from 0 to 59, a space, then a number from 0 to 14.')
+
+def showInstructions():
+	print('''Instructions:
+
+	You are the captain of the Virgil, a treasure-hunting ship. Your current
+	mission is to use sonar beacons to find three sunken treasure chests at 
+	the bottom of the ocean. But you only have cheap beacons that finds 
+	distance, not direction.
+	
+	Enter the coordinates to drop a sonar beacon. The ocean map will be 
+	marked with how far away the nearest chest is, or an X if it is beyond 
+	the sonar beacon's range. For example, the C marks are where chests are.
+	The sonar beacon shows a 3 because the closest chest is 3 spaces away:
+
+                     1         2         3
+           012345678901234567890123456789012
+
+         0 ~~~~`~```~`~``~~~``~`~~``~~~``~`~ 0
+         1 ~`~`~``~~`~```~~~```~~`~`~~~`~~~~ 1
+         2 `~`C``3`~~~~`C`~~~~`````~~``~~~`` 2
+         3 ````````~~~`````~~~`~`````~`~``~` 3
+         4 ~`~~~~`~~`~~`C`~``~~`~~~`~```~``~ 4
+
+           012345678901234567890123456789012
+                     1         2         3
+
+	(In the real game, the chests are not visible in the ocean.)
+
+	Press Enter to continue...''')
+	input()
+
+	print('''
+	When you drop a sonar beacon directly on a chest, you retrieve it 
+	and the other sonar beacons update to show how far away the next nearest 
+	chest is. The chests are beyond the range of the sonar becaon on the left, 
+	so it shows an X.
+
+                    1         2         3
+          012345678901234567890123456789012
+        0 ~~~~`~```~`~``~~~``~`~~``~~~``~`~ 0
+        1 ~`~`~``~~`~```~~~```~~`~`~~~`~~~~ 1
+        2 `~`X``7`~~~~`C`~~~~`````~~``~~~`` 2
+        3 ````````~~~`````~~~`~`````~`~``~` 3
+        4 ~`~~~~`~~`~~`C`~``~~`~~~`~```~``~ 4
+          012345678901234567890123456789012
+                    1         2         3
+
+	The treasure chests don't move around. Sonar devices can detect treasure
+  	chests up to a distance of 9 spaces. Try to collect all 3 chests before
+  	running out of sonar beacons. Good luck!
+		
+	Press enter to continue...''')
+	input()
+
+
+
+print('S O N A R !')
+print()
+print('Would you like to view the instructions? (Yes/No)')
+if input().lower().startswith('y'):
+	showInstructions()
+
+while True:
+	# Game setup
+	sonarDevices = 20
+	theBoard = getNewBoard()
+	theChests = getRandomChests(3)
+	drawBoard(theBoard)
+	previousMoves = []
+
+	while sonarDevices > 0:
+		# Show sonar beacon and chest status
+		print()
+		print('You have %s sonar beacon(s) left. %s treasure chest(s) \
+remaining.' % (sonarDevices, len(theChests)))
+
+		x, y = enterPlayerMove(previousMoves)
+		previousMoves.append([x, y]) # We must track all moves so that sonar 
+		# beacons can be updated.
+
+		moveResult = makeMove(theBoard, theChests, x, y)
+		if moveResult == False:
+			continue
+		else:
+			if moveResult == 'You have found a sunken treasure chest!':
+				# Update all the sonar devices currently on the map.
+				for x, y in previousMoves:
+					makeMove(theBoard, theChests, x, y)
+			drawBoard(theBoard)
+			print(moveResult)
+
+		if len(theChests) == 0:
+			print('Congratulations! You have found all the sunken treasure \
+chests.')
+			break
+
+		sonarDevices -= 1
+
+	if sonarDevices == 0:
+		print('We\'ve run out of sonar beacons! Now we have to turn the ship \
+around and head')
+		print('for home with the treasure chests still out there! Game over.')
+		print('   The remaining chests were here:')
+		for x, y in theChests:
+			print('   %s, %s' % (x, y))
+			print('Do you want to play again? (yes or no)')
+			if not input().lower().startswith('y'):
+				sys.exit()
